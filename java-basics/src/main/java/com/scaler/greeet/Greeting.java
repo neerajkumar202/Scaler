797f9c44dbc8ce91ab778ec4d698bc91f9374991
Neerajkumar <neerajkumar97837@gmail.com>
2023-10-30 12:26:25 +0530

    there is an error occure while running on terminal: no main manifest attribute, cleared the by adding the main class path in the build.gradle the main file

---
+++ java-basics/src/main/java/com/scaler/greeet/Greeting.java
@@ -0,0 +1,19 @@
+//package com.scaler;
+
+public class Greeting {
+    /*
+    it generate the greeting based on the time of the day
+    */
+    public String greeting(){
+        int hour = java.time.LocalTime.now().getHour();
+        if(hour >= 0 && hour < 12 ){
+            return "Good Morning";
+        } else if (hour >= 12 && hour < 16) {
+            return  "Good Afternoon";
+
+        }
+        else {
+            return "Good Evening";
+        }
+    }
+}
